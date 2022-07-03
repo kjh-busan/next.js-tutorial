881e0171c18482d383ae5a2afccf4ad73ef708cf
egoing <egoing@gmail.com>
2022-07-04 00:16:17 +0900

    14:36 ~ 19:28 - 환경변수 사용법

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <li><a href="/sub/about">/pages/sub/about.js</a></li>
         <li><a href="/sub/1">/pages/sub/[id].js</a></li>
         <li><a href="/sub/2">/pages/sub/[id].js</a></li>
+        <li><a href="/sub/fetch">/page/sub/fetch.js</a></li>
       </ul>
     </div>
   )
